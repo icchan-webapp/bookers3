cc5b5f0587b4117f4e5a241dfab4e473d566ea4f
EC2 Default User <ec2-user@ip-172-31-25-255.ec2.internal>
2022-03-03 05:56:49 +0000

    first submit

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   patch 'books/:id' => 'books#update', as: 'update_book'
   patch 'users/:id' => 'users#update', as: 'update_user'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
   
-  resources :books, only: [:index, :show, :create, :edit, :update, :destroy
-  ]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
